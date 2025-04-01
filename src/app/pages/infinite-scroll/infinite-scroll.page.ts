4787bbcb4b563fdff5fcd7f53d0fcf83f4a4b554
tilordqwerty <elgabrielcisneros@gmail.com>
2025-04-01 12:53:26 -0400

    feature: add Infinite Scroll page at pages/infinite-scroll, formatted grid/grid.page.html, grid/grid.page.ts files with prettier vscode extension.

--- src/app/pages/infinite-scroll/infinite-scroll.page.ts
+++ src/app/pages/infinite-scroll/infinite-scroll.page.ts
@@ -1,20 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonList,
+  IonLabel,
+  IonItem,
+  IonAvatar,
+  IonInfiniteScroll,
+} from '@ionic/angular/standalone';
+import { HeaderComponent } from '../../components/header/header.component';
+import { IonInfiniteScrollCustomEvent } from '@ionic/core';
 
 @Component({
   selector: 'app-infinite-scroll',
   templateUrl: './infinite-scroll.page.html',
   styleUrls: ['./infinite-scroll.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonInfiniteScroll,
+    IonAvatar,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonContent,
+    CommonModule,
+    FormsModule,
+    HeaderComponent,
+  ],
 })
 export class InfiniteScrollPage implements OnInit {
+  
+  @ViewChild(IonInfiniteScroll) infiniteScroll!: IonInfiniteScroll
+  
+  users: any[] = Array(20);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  private generateUsers() {
+    const newUsers = Array(20);
+
+    this.users.push(...newUsers);
   }
 
+  loadUsers(event: IonInfiniteScrollCustomEvent<void>) {
+    
+    console.log(event);
+
+    
+    setTimeout(() => {
+    
+      if(this.users.length > 100){
+        
+        this.infiniteScroll.complete();
+        this.infiniteScroll.disabled = true
+        return;
+        
+      }
+      
+      this.generateUsers();
+      
+      this.infiniteScroll.complete();
+    }, 1500);
+  }
 }
