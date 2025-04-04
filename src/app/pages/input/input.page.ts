acc782ce7f444366ba1c8ba5c742868ad365eaca
tilordqwerty <elgabrielcisneros@gmail.com>
2025-04-04 17:03:23 -0400

    feature: create an example input and a input form to catch email and password at input/input.page.html and input/input.page.ts

--- src/app/pages/input/input.page.ts
+++ src/app/pages/input/input.page.ts
@@ -1,20 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormsModule, NgForm } from '@angular/forms';
+import { IonContent, IonList, IonItem, IonInput, IonLabel, IonListHeader, IonButton, IonIcon, IonButtons } from '@ionic/angular/standalone';
+import { HeaderComponent } from "../../components/header/header.component";
+import { addIcons } from 'ionicons';
+import { eye, eyeOff, eyeOffOutline, eyeOutline, add } from 'ionicons/icons';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.page.html',
   styleUrls: ['./input.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButtons, IonIcon, IonButton, IonListHeader, IonLabel, IonInput, IonItem, IonList, IonContent, CommonModule, FormsModule, HeaderComponent]
 })
 export class InputPage implements OnInit {
+  
+  name: string = 'Gabriel Cisneros';
+  
+  user = {
+    email: '',
+    password: ''
+  }
 
-  constructor() { }
+  eye: boolean = false;
+
+  constructor() {
+    addIcons({add,eyeOffOutline,eyeOutline});
+  }
 
   ngOnInit() {
+  }
+
+  submitEmail(formInput: NgForm){
+    console.log('submit')
+    console.log(this.user);
+    console.log(formInput)
+  }
+
+  showPassword() {
+    
+    this.eye = !this.eye;
+    
+    const input = document.querySelector('ion-input[type="password"]') as HTMLInputElement;
+    
+    if (this.eye) {
+      input.type = 'text';
+    }
+    else {
+      input.type = 'password';
+    }
+
+    
   }
 
 }
